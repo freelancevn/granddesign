6dde9a2f826ecab66218e7cc2921e29769348ba9
penguinsh <tuanhuy1291989@gmail.com>
2014-03-20 01:20:08 +0700

    Add 1 news for News page

--- data/default/templates_c/%%D8^D8B^D8BCAF8F%%news.phtml.php
+++ data/default/templates_c/%%D8^D8B^D8BCAF8F%%news.phtml.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.20, created on 2014-03-19 00:04:47
+<?php /* Smarty version 2.6.20, created on 2014-03-20 01:10:10
          compiled from public/news.phtml */ ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => 'common/header.phtml', 'smarty_include_vars' => array()));
@@ -13,7 +13,24 @@ unset($_smarty_tpl_vars);
 		and on all orders over &pound;500** <strong>Free returns</strong> on
 		all orders
 	</div>
+	
+	<div class="nw1 nh2 newsImage">
+		<img src="<?php echo @IMAGES; ?>
+news/mh370.jpg" />
+		
+	</div>
+	
+	<div class="w2 nh1 black newsTitle">
+		<strong>Malaysia bác tin MH370 xuất hiện ở Maldives</strong>
+	</div>
+	
+	<div class="w2 nh1 newsSum">
+		Bộ trưởng Quốc phòng Malaysia hôm nay cho biết Maldives xác nhận thông tin về việc người dân thấy phi cơ lớn nghi là MH370 xuất hiện trong không phận quốc đảo này là không chính xác.
+	</div>
+
 </div>
+
+
 
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => 'common/footer.phtml', 'smarty_include_vars' => array()));
