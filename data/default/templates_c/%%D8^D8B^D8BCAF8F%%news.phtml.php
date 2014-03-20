f0c3cb1dc06337256908efb442e7b9037c3a71b3
penguinsh <tuanhuy1291989@gmail.com>
2014-03-21 01:38:28 +0700

    Add one news

--- data/default/templates_c/%%D8^D8B^D8BCAF8F%%news.phtml.php
+++ data/default/templates_c/%%D8^D8B^D8BCAF8F%%news.phtml.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.20, created on 2014-03-20 01:10:10
+<?php /* Smarty version 2.6.20, created on 2014-03-21 01:34:16
          compiled from public/news.phtml */ ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => 'common/header.phtml', 'smarty_include_vars' => array()));
@@ -14,19 +14,34 @@ unset($_smarty_tpl_vars);
 		all orders
 	</div>
 	
-	<div class="nw1 nh2 newsImage">
-		<img src="<?php echo @IMAGES; ?>
+		<div class="nw1 nh2 newsImage">
+			<img src="<?php echo @IMAGES; ?>
 news/mh370.jpg" />
+			
+		</div>
 		
-	</div>
-	
-	<div class="w2 nh1 black newsTitle">
-		<strong>Malaysia bác tin MH370 xuất hiện ở Maldives</strong>
-	</div>
-	
-	<div class="w2 nh1 newsSum">
-		Bộ trưởng Quốc phòng Malaysia hôm nay cho biết Maldives xác nhận thông tin về việc người dân thấy phi cơ lớn nghi là MH370 xuất hiện trong không phận quốc đảo này là không chính xác.
-	</div>
+		<div class="w2 nh1 black newsTitle">
+			<strong>Malaysia bác tin MH370 xuất hiện ở Maldives</strong>
+		</div>
+		
+		<div class="w2 nh3 newsSum">
+			Bộ trưởng Quốc phòng Malaysia hôm nay cho biết Maldives xác nhận thông tin về việc người dân thấy phi cơ lớn nghi là MH370 xuất hiện trong không phận quốc đảo này là không chính xác.
+		</div>
+		
+		<div class="nw1 nh2 newsImage">
+			<img src="<?php echo @IMAGES; ?>
+news/vatthe.jpg" />
+			
+		</div>
+		
+		<div class="w2 nh1 black newsTitle">
+			<strong>Radar Mỹ phát hiện vật thể 'cỡ lớn' dưới biển</strong>
+		</div>
+		
+		<div class="w2 nh3 newsSum">
+			Radar trên một máy bay trinh sát Mỹ phát hiện một thứ "cỡ lớn" dưới mặt nước phía nam Ấn Độ Dương, sau khi vệ tinh chụp được những vật thể có thể là mảnh vỡ của chuyến bay MH370 mất tích. 
+		</div>
+		
 
 </div>
 
