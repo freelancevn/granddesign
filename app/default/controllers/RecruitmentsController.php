60c326e71e3de6b9ad0d38ed65246683ec033837
leotimus <hatrongdai@gmail.com>
2014-06-07 18:25:11 +0700

    fix recruitment.

--- app/default/controllers/RecruitmentsController.php
+++ app/default/controllers/RecruitmentsController.php
@@ -2,15 +2,11 @@
 class RecruitmentsController extends Zend_Controller_Action {
     public function indexAction()
     {
+    	include_once('common/header.php');
         $recruitment = new Recruitment();
         $this->view->recruitmentlist = $recruitment->getAllVisibleRecruitment();
     }
 
-    public function recruitmentsAction()
-    {
-        $recruitment = new Recruitment();
-        $this->view->recruitmentlist = $recruitment->getAllVisibleRecruitment();
-    }
 }
 
 ?>
